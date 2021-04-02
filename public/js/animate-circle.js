630e6f74193009908d200906e67d6cd7d44e6336
Ivan <pig1995z@gmail.com>
2021-04-02 16:11:20 +0300

    rebuild from Bohdan

--- public/js/animate-circle.js
+++ public/js/animate-circle.js
@@ -69,7 +69,7 @@ function spawnColor(iter) {
       );
       document.querySelector(
         ".js-guage-svg > path:nth-child(" + String(this.iteration) + ")"
-      ).style.fill = "#44BCFF";
+      ).style.fill = "#fb9b2b";
       this.color = this.color - rgbRedValue / 20;
       this.goFrames = this.goFrames + 1;
       this.reqAnim = requestAnimationFrame(this.fillColor.bind(this));
--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import "./university/reset.css";
 import "./university/animate.css";
-import "./university/fonts.css";
 import "./university/style.css";
-import "./university/js/miniature-libs.css";
+
 
 ReactDOM.render(
   <React.StrictMode>
