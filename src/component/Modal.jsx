ed7bea4d1a03f5184298a12c05aaf5f6dc8a746f
Ivan <pig1995z@gmail.com>
2021-04-03 02:12:40 +0300

    add route

---
+++ src/component/Modal.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {
+   BrowserRouter as Router,
+   Switch,
+   Route,
+   Link,
+ } from "react-router-dom";
+
+const Modal = () => {
+  return (
+    <div className="content">
+      <Link to="/"> test</Link>
+    </div>
+  );
+};
+
+export default Modal;
