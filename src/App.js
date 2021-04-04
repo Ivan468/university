d6c825e98661d9955c2fec8f25d7612256f2d7aa
Ivan <pig1995z@gmail.com>
2021-04-04 21:03:24 +0300

    color hologram

--- src/App.js
+++ src/App.js
@@ -23,8 +23,7 @@ function App() {
             viewBox="0 0 20 29"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
-          >
-            <circle cx="10" cy="5" r="4" stroke="#21CAFF" strokeWidth="2" />
+          >            <circle cx="10" cy="5" r="4" stroke="#21CAFF" strokeWidth="2" />
             <path
               d="M1.86852 15L10 27.1972L18.1315 15L1.86852 15Z"
               stroke="#21CAFF"
