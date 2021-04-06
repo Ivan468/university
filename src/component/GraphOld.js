d4bcf5001ae04cba816db31f8829ee33a4ffe52d
Ivan <pig1995z@gmail.com>
2021-04-06 23:41:11 +0300

    add new design - twi bg bloks

---
+++ src/component/GraphOld.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+const GraphOld = () => {
+  return (
+    <div className="retancle-one-graph">
+      <div className="rotate-left">
+        <div>
+          <p>Севастопольський</p>
+          <div className="retalncle"></div>
+        </div>
+        <div>
+          <p>Тульский</p>
+          <div className="retalncle tulskiy-un"></div>
+        </div>
+        <div>
+          <p>Волгоградский</p>
+          <div className="retalncle volgradskiy-un"></div>
+        </div>
+        <div>
+          <p>Севастопольский</p>
+          <div className="retalncle sevastopolskiy-un"></div>
+        </div>
+        <div>
+          <p>Тульский</p>
+          <div className="retalncle tulskiy-un-two"></div>
+        </div>
+        <div>
+          <p>Севастопольский</p>
+          <div className="retalncle sevastopolskiy-un-two"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GraphOld;
